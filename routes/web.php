6db231b41920a1bb4340ab2f6d9bed25b8486d99
MitchelRos <113449451+MitchelRos@users.noreply.github.com>
2023-05-22 03:44:27 +0200

    añadir las rutas (#2)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ClientesController;
+use App\Http\Controllers\Admin\FacturacionController;
+use App\Http\Controllers\Admin\ContabilidadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +15,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// Ruta de inicio?
+Route::get('/', function () {return view('welcome');});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rutas de Clientes
+Route::get('/admin/clientes', [ClientesController::class, 'index'])->name('admin.clientes.index');
+
+// Rutas de Facturación
+Route::get('/admin/facturacion', [FacturacionController::class, 'index'])->name('admin.facturacion.index');
+
+// Rutas de Contabilidad
+Route::get('/admin/contabilidad', [ContabilidadController::class, 'index'])->name('admin.contabilidad.index');
+
